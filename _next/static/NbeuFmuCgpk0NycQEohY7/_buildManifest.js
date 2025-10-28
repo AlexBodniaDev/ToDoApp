7ac84b5bece9e117a3acc73bcfccea405e50375d
AlexBodniaDev <bodniaalex2911@gmail.com>
2025-10-28 21:03:52 +0200

    Updates

---
+++ _next/static/NbeuFmuCgpk0NycQEohY7/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ToDoApp//_next/:path+",
+        "destination": "/ToDoApp/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
